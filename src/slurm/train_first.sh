d49d12789a3cac92fd979d398f7190cd458ef833
erikhenicke <mail@ehenicke.de>
2026-02-05 14:16:18 +0100

    Slurm-train script: request gpu

--- src/slurm/train_first.sh
+++ src/slurm/train_first.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #SBATCH --partition=robolab
-#SBATCH --job-name=download_landsat8
+#SBATCH --job-name=train_deit
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=8
-#SBATCH --output=download_landsat8.%j.out
-#SBATCH --error=download_landsat8.%j.err
+#SBATCH --cpus-per-task=4
+#SBATCH --output=train_deit.%j.out
+#SBATCH --error=train_deit.%j.err
+#SBATCH --gres=gpu:1
   
 # Your commands go here
 cd /home/henicke/git/bigpicture
-PYTHONPATH=/home/henicke/git/bigpicture/src /home/henicke/miniconda3/envs/wilds/bin/python /home/henicke/git/bigpicture/src/train/train_first.py
+PYTHONPATH=/home/henicke/git/bigpicture/src /home/henicke/miniconda3/envs/wilds/bin/python /home/henicke/git/bigpicture/src/train/train_first.py --model=multi --epochs=1 --batch_size=32 --frac=0.01
