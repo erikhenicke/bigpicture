87ddfa42b3578a332016b2c5deb6ebdea777622b
ru84rin <erik.henicke@uni-jena.de>
2026-02-26 21:04:09 +0100

    Model training: Traing single densenet with checkpoints.

--- src/slurm/train_single_densenet.sh
+++ src/slurm/train_single_densenet.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --partition=robolab
 #SBATCH --exclude=gaia1,gaia2,gaia3,gaia6,gaia7
-#SBATCH --job-name=train_single_dense_net_121
+#SBATCH --job-name=train_single_densenet
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=4
-#SBATCH --output=slurm/train_single_dense_net_121.%j.out
-#SBATCH --error=slurm/train_single_dense_net_121.%j.err
+#SBATCH --output=slurm/train_single_densenet.%j.out
+#SBATCH --error=slurm/train_single_densenet.%j.err
 #SBATCH --gres=gpu:1
   
 # Your commands go here
 cd /home/henicke/git/bigpicture
-PYTHONPATH=/home/henicke/git/bigpicture/src /home/henicke/miniconda3/envs/wilds2/bin/python /home/henicke/git/bigpicture/src/train/run_experiment.py --model_type=single-densenet --epochs=100 --batch_size=64 --frac=1.0 --optimizer=adam --learning_rate=1e-4 --learning_rate_decay=0.96
-PYTHONPATH=/home/henicke/git/bigpicture/src /home/henicke/miniconda3/envs/wilds2/bin/python /home/henicke/git/bigpicture/src/train/run_experiment.py --model_type=single-densenet --epochs=100 --batch_size=64 --frac=1.0 --optimizer=adamw --learning_rate=1e-5 --weight_decay=0.01 
+PYTHONPATH=/home/henicke/git/bigpicture/src /home/henicke/miniconda3/envs/wilds2/bin/python /home/henicke/git/bigpicture/src/train/run_experiment.py --model_type=single-densenet --image_net=hr --epochs=50 --batch_size=64 --frac=1 --optimizer=adam --learning_rate=1e-4 --learning_rate_scheduler=step --learning_rate_decay=0.96
+PYTHONPATH=/home/henicke/git/bigpicture/src /home/henicke/miniconda3/envs/wilds2/bin/python /home/henicke/git/bigpicture/src/train/run_experiment.py --model_type=single-densenet --image_net=none --epochs=50 --batch_size=64 --frac=1 --optimizer=adam --learning_rate=1e-4 --learning_rate_scheduler=step --learning_rate_decay=0.96
