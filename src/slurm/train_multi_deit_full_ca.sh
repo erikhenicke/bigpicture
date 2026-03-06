fae822a3be5fcd910b5584a195a92a665b16e847
erikhenicke <mail@ehenicke.de>
2026-03-06 13:12:49 +0100

    Model training: Reduce batch size for cross attention model.

--- src/slurm/train_multi_deit_full_ca.sh
+++ src/slurm/train_multi_deit_full_ca.sh
@@ -10,5 +10,5 @@
   
 # Your commands go here
 cd /home/henicke/git/bigpicture
-PYTHONPATH=/home/henicke/git/bigpicture/src /home/henicke/miniconda3/envs/wilds2/bin/python /home/henicke/git/bigpicture/src/train/run_experiment.py --model_type=multi-deit-cross-attn --cross_attention_depths 2 5 8 11 --data_augmentation --image_net=both --epochs=50 --batch_size=384 --frac=1.0 --optimizer=adam --learning_rate=1e-4 --learning_rate_scheduler=plateau --learning_rate_decay=0.5 --plateau_patience=5
+PYTHONPATH=/home/henicke/git/bigpicture/src /home/henicke/miniconda3/envs/wilds2/bin/python /home/henicke/git/bigpicture/src/train/run_experiment.py --model_type=multi-deit-cross-attn --cross_attention_depths 2 5 8 11 --data_augmentation --image_net=both --epochs=50 --batch_size=256 --frac=1.0 --optimizer=adam --learning_rate=1e-4 --learning_rate_scheduler=plateau --learning_rate_decay=0.5 --plateau_patience=5
 
