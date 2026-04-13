d9764595d51243e4d6ea3518c56fb82672bbed6b
ru84rin <erik.henicke@uni-jena.de>
2026-04-13 18:50:01 +0200

    Fix experiment script with hydra.

--- src/train/run_experiment_lightning_hydra.py
+++ src/train/run_experiment_lightning_hydra.py
@@ -10,7 +10,7 @@ from omegaconf import DictConfig, OmegaConf
 from torch.utils.data import DataLoader
 import wandb
 
-from datasets.fmow_multiscale import FMoWMultiScaleDataset
+from dataset.fmow_multiscale_dataset import FMoWMultiScaleDataset
 from models.late_fusion import LateFusionModule
 from train.utils import make_multiscale_dataset, make_multiscale_loader, resolve_preprocessed_dir
 
