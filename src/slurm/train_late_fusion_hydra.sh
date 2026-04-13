12e2125c0a062e8ce96bbc28f7006821c53b8e9c
ru84rin <erik.henicke@uni-jena.de>
2026-04-13 18:39:32 +0200

    Run experiments with hydra.

---
+++ src/slurm/train_late_fusion_hydra.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --partition=robolab
+#SBATCH --nodelist=gaia4,gaia5
+#SBATCH --job-name=train_late_fusion_hydra
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --output=slurm/train_late_fusion_hydra.%j.out
+#SBATCH --error=slurm/train_late_fusion_hydra.%j.err
+#SBATCH --gres=gpu:1
+
+cd /home/henicke/git/bigpicture
+uv run --env-file .env src/train/run_experiment_lightning_hydra.py
