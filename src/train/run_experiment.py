8fbc8eddc75fe6f43e159f0402ca0d2df34559d5
ru84rin <erik.henicke@uni-jena.de>
2026-04-17 18:14:32 +0200

    Fix checkpoint structure.

--- src/train/run_experiment.py
+++ src/train/run_experiment.py
@@ -148,8 +148,7 @@ def _run_once(
     seed_everything(cfg.seed + run_idx, workers=True)
 
     run_name = f"{cfg.wandb.run_name}-run{run_idx}"
-    checkpoint_dir = f"{cfg.checkpoint_dir}/{cfg.wandb.run_name}/run{run_idx}"
-    print(f"Starting run {run_idx} with checkpoint dir: {checkpoint_dir}")
+    checkpoint_dir = f"{default_root_dir}/checkpoints/run{run_idx}"
 
     wandb_logger = WandbLogger(
         project=cfg.wandb.project,
