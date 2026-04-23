371d055a73bd6380b877cf75eccdb81898a47452
erikhenicke <mail@ehenicke.de>
2026-04-23 17:20:40 +0200

    Rerun concat experiment with densenet

--- src/slurm/train_multi_densenet.sh
+++ src/slurm/train_multi_densenet.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --partition=robolab
-#SBATCH --exclude=gaia1,gaia2,gaia3,gaia6,gaia7
+#SBATCH --nodelist=gaia4,gaia5,nyx
 #SBATCH --job-name=train_multi_densenet
 #SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=8GB
 #SBATCH --output=slurm/train_multi_densenet.%j.out
 #SBATCH --error=slurm/train_multi_densenet.%j.err
-#SBATCH --gres=gpu:1
   
 # Your commands go here
 cd /home/henicke/git/bigpicture
-PYTHONPATH=/home/henicke/git/bigpicture/src /home/henicke/miniconda3/envs/wilds2/bin/python /home/henicke/git/bigpicture/src/train/run_experiment.py --model_type=multi-densenet --data_augmentation --image_net=both --epochs=50 --batch_size=64 --frac=1 --optimizer=adam --learning_rate=1e-4 --learning_rate_scheduler=step --learning_rate_decay=0.96
-PYTHONPATH=/home/henicke/git/bigpicture/src /home/henicke/miniconda3/envs/wilds2/bin/python /home/henicke/git/bigpicture/src/train/run_experiment.py --model_type=multi-densenet --data_augmentation --image_net=hr --epochs=50 --batch_size=64 --frac=1 --optimizer=adam --learning_rate=1e-4 --learning_rate_scheduler=step --learning_rate_decay=0.96
-# PYTHONPATH=/home/henicke/git/bigpicture/src /home/henicke/miniconda3/envs/wilds2/bin/python /home/henicke/git/bigpicture/src/train/run_experiment.py --model_type=multi-densenet --data_augmentation --image_net=none --epochs=50 --batch_size=64 --frac=1 --optimizer=adam --learning_rate=1e-4 --learning_rate_scheduler=step --learning_rate_decay=0.96
+srun uv run --env-file .env src/train/run_experiment.py --model_type=multi-densenet --data_augmentation --image_net=both --epochs=50 --batch_size=64 --frac=1 --optimizer=adam --learning_rate=1e-4 --learning_rate_scheduler=step --learning_rate_decay=0.96
