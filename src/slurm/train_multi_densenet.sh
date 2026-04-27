87a8d55fb532f0f4947e192143d3408a54f220a3
erikhenicke <mail@ehenicke.de>
2026-04-27 11:10:21 +0200

    Run script from correct workdir.

--- src/slurm/train_multi_densenet.sh
+++ src/slurm/train_multi_densenet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=robolab
-#SBATCH --nodelist=gaia4,gaia5,nyx
+#SBATCH --nodelist=gaia4,gaia5,gaia7
 #SBATCH --job-name=train_multi_densenet
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -11,5 +11,5 @@
 #SBATCH --error=slurm/train_multi_densenet.%j.err
   
 # Your commands go here
-cd /home/henicke/git/bigpicture
+cd /home/henicke/git/bp-exp
 srun uv run --env-file .env src/train/run_experiment.py --model_type=multi-densenet --data_augmentation --image_net=both --epochs=50 --batch_size=64 --frac=1 --optimizer=adam --learning_rate=1e-4 --learning_rate_scheduler=step --learning_rate_decay=0.96
