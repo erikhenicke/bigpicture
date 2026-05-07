5f38e9ebba07f74f381a88454d575ec8a05751be
ru84rin <erik.henicke@uni-jena.de>
2026-05-07 10:47:13 +0200

    Copy renormalized images to gaia4 and gaia5

--- src/slurm/copy_preprocessed_gaia4.sh
+++ src/slurm/copy_preprocessed_gaia4.sh
@@ -3,10 +3,12 @@
 #SBATCH --nodelist=gaia4
 #SBATCH --job-name=copy_fmow_to_gaia4
 #SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=4
 #SBATCH --output=log/slurm/copy_fmow_to_gaia4.%j.out
 #SBATCH --error=log/slurm/copy_fmow_to_gaia4.%j.err
 
 set -euo pipefail
 
-rsync -avh --progress /home/datasets4/FMoW_LandSat/fmow_preprocessed_norm /data/henicke/FMoW_LandSat/
+srun rsync -avh --progress gaia7:/data/henicke/FMoW_LandSat_Norm_/fmow_preprocessed/landsat /data/henicke/FMoW_LandSat_Norm_/fmow_preprocessed/
 
