700c6e2968e8315820825397fbca74b66f7e60d0
ru84rin <erik.henicke@uni-jena.de>
2026-05-08 01:44:28 +0200

    Add new experiments.

--- src/results/eval_experiments.py
+++ src/results/eval_experiments.py
@@ -97,6 +97,8 @@ def format_experiment_name(
     for param_key, value in overrides.items():
         print(param_key, value)
         param_trans = translations["params"].get(param_key)
+        if param_trans and param_trans.get("hidden", False):
+            continue
         label = param_trans[symbol_key] if param_trans else param_key.split(".")[-1]
         no_value = param_trans.get("no_value", False) if param_trans else False
         if isinstance(value, bool):
