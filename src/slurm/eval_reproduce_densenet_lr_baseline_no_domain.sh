317704521b1265fcfb4a7aa6c2675ad619bf0e29
erikhenicke <mail@ehenicke.de>
2026-06-02 16:25:13 +0200

    Reproduce test metrics form checkpoints

---
+++ src/slurm/eval_reproduce_densenet_lr_baseline_no_domain.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --partition=robolab
+#SBATCH --nodelist=gaia4,gaia5
+#SBATCH --job-name=reproduce_densenet_lr_baseline_no_domain
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=8GB
+#SBATCH --output=log/slurm/reproduce_densenet_lr_baseline_no_domain.%j.out
+#SBATCH --error=log/slurm/reproduce_densenet_lr_baseline_no_domain.%j.err
+cd /home/henicke/git/bigpicture
+srun uv run --env-file .env src/results/eval_reproduce.py --config src/train/configs/run/baselines.yaml --run-name densenet_lr_baseline_no_domain
