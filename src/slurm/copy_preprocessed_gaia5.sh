4dbacccf1a9a7872c01e1c1274a5ca5725faa9da
erikhenicke <mail@ehenicke.de>
2026-06-11 15:02:03 +0200

    Correct reproduce script by setting defaults to new config keys.

--- src/slurm/copy_preprocessed_gaia5.sh
+++ src/slurm/copy_preprocessed_gaia5.sh
@@ -10,6 +10,4 @@
 
 set -euo pipefail
 
-mkdir -p /data/henicke/FMoW_LandSat_/fmow_preprocessed/
-ln -s /data/henicke/FMoW_LandSat_Norm/fmow_preprocessed/fmow_rgb /data/henicke/FMoW_LandSat_/fmow_preprocessed/fmow_rgb
-srun rsync -avh --progress /home/datasets4/FMoW_LandSat/fmow_preprocessed/landsat /data/henicke/FMoW_LandSat_/fmow_preprocessed/
+srun rsync -avh --progress gaia4:/data/henicke/FMoW_LandSat_Norm/fmow_preprocessed/fmow_rgb /data/henicke/FMoW_LandSat_FullRes/fmow_preprocessed/
