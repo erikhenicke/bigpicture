77ff2403d729cd19e9ef9aec0afdbc2a32a432f5
ru84rin <erik.henicke@uni-jena.de>
2026-06-11 14:40:11 +0200

    Reproduce spatial encoding experiment results

--- src/slurm/eval_reproduce.sh
+++ src/slurm/eval_reproduce.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=robolab
-#SBATCH --nodelist=gaia4,gaia5,gaia7
+#SBATCH --nodelist=gaia4,gaia7
 #SBATCH --job-name=reproduce
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -10,9 +10,9 @@
 #SBATCH --output=log/slurm/reproduce.%j.out
 #SBATCH --error=log/slurm/reproduce.%j.err
 cd /home/henicke/git/bigpicture
-srun uv run --env-file .env src/results/eval_reproduce.py --config src/train/configs/run/baselines.yaml --run-name densenet_baseline
-srun uv run --env-file .env src/results/eval_reproduce.py --config src/train/configs/run/baselines.yaml --run-name densenet_lr_baseline
-srun uv run --env-file .env src/results/eval_reproduce.py --config src/train/configs/run/baselines.yaml --run-name densenet_lr_baseline_no_domain
-srun uv run --env-file .env src/results/eval_reproduce.py --config src/train/configs/run/baselines.yaml --run-name dinov3_baseline
-srun uv run --env-file .env src/results/eval_reproduce.py --config src/train/configs/run/baselines.yaml --run-name dinov3_lr_baseline
-srun uv run --env-file .env src/results/eval_reproduce.py --config src/train/configs/run/baselines.yaml --run-name dinov3_lr_baseline_no_domain
+srun uv run --env-file .env src/results/eval_reproduce.py --config src/train/configs/run/multsim.yaml --run-name film_om_gauss_pe_freq
+srun uv run --env-file .env src/results/eval_reproduce.py --config src/train/configs/run/multsim.yaml --run-name film_detach
+srun uv run --env-file .env src/results/eval_reproduce.py --config src/train/configs/run/multsim.yaml --run-name film_no_domain
+srun uv run --env-file .env src/results/eval_reproduce.py --config src/train/configs/run/multsim.yaml --run-name film
+srun uv run --env-file .env src/results/eval_reproduce.py --config src/train/configs/run/multsim.yaml --run-name film_pe_freq
+srun uv run --env-file .env src/results/eval_reproduce.py --config src/train/configs/run/multsim.yaml --run-name film_om_gauss
