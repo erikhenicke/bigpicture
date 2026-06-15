bed378892034ca7075a0427769ce4af8b1223f59
ru84rin <erik.henicke@uni-jena.de>
2026-06-15 17:41:15 +0200

    Copy extracted features satclip l=40 to gaias.

--- src/slurm/copy_features_gaia4.sh
+++ src/slurm/copy_features_gaia4.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env bash
 #SBATCH --partition=robolab
 #SBATCH --nodelist=gaia4
-#SBATCH --job-name=copy_features_to_gaia4
+#SBATCH --job-name=copy_features_from_gaia4
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --output=log/slurm/copy_features_to_gaia4.%j.out
-#SBATCH --error=log/slurm/copy_features_to_gaia4.%j.err
+#SBATCH --output=log/slurm/copy_features_from_gaia4.%j.out
+#SBATCH --error=log/slurm/copy_features_from_gaia4.%j.err
 
 set -euo pipefail
 
-for dir in $(ssh gaia7 'ls -d /data/henicke/*Satclip*Features'); do
-    mkdir -p "${dir}"
-    srun rsync -avh --progress "gaia7:${dir}/" "${dir}/"
+for dir in $(ls -d /data/henicke/*Satclip*40*Features); do
+    srun rsync -avh --progress "${dir}/" "gaia5:${dir}/" 
+    srun rsync -avh --progress "${dir}/" "gaia7:${dir}/" 
 done
-
-# for dir in $(ssh gaia5 'ls -d /data/henicke/*Features'); do
-#     mkdir -p "${dir}"
-#     srun rsync -avh --progress "gaia5:${dir}/" "${dir}/"
-# done
