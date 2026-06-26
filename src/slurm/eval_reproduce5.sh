d416d5922eba7dbfa9c7d27bdce8c81f9587df49
erikhenicke <mail@ehenicke.de>
2026-06-26 08:48:52 +0200

    Update run logs

---
+++ src/slurm/eval_reproduce5.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --partition=robolab
+#SBATCH --nodelist=gaia5
+#SBATCH --job-name=reproduce
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=8GB
+#SBATCH --output=log/slurm/reproduce.%j.out
+#SBATCH --error=log/slurm/reproduce.%j.err
+cd /home/henicke/git/bigpicture
+
+srun uv run --env-file .env src/results/eval_reproduce.py --config src/train/configs/run/spatial_extent.yaml --run-name film_om_gauss_pe_freq_lr_crop_10km 
